9726dccce38d419980f20478cadd5b39d4bbfc1b
Sai Gowtham Talluri <saigowtham.t1299@gmail.com>
2025-10-28 13:39:33 -0500

    updated tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main style={{ textAlign: "center", padding: "2rem" }}>
-      <h1>Welcome to My Website!</h1>
-      <p>This is my custom homepage deployed on Netlify 🎉</p>
+      <h1></h1>
+      <p></p>
     </main>
   );
 }
